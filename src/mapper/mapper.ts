1b367bc6c25575c735330c6abd46708e0cfca833
Armen Mesropyan <amesropyan@bangits.com>
2021-11-23 11:31:04 +0400

    Updated automapper core imports

--- src/mapper/mapper.ts
+++ src/mapper/mapper.ts
@@ -1,9 +1,11 @@
 import { classes } from '@automapper/classes';
-import { CamelCaseNamingConvention, createMapper } from '@automapper/core';
+import autoMapper from '@automapper/core';
 import { baseProfile } from './profile';
 
-export const mapper = createMapper({
-  name: 'AuthMapper',
-  pluginInitializer: classes,
-  namingConventions: new CamelCaseNamingConvention()
-}).addProfile(baseProfile);
+export const mapper = autoMapper
+  .createMapper({
+    name: 'AuthMapper',
+    pluginInitializer: classes,
+    namingConventions: new autoMapper.CamelCaseNamingConvention()
+  })
+  .addProfile(baseProfile);
