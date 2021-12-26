5b5450eed2119e336ce406b50e016710f537b047
Armen Mesropyan <amesropyan@bangits.com>
2021-12-27 01:34:31 +0400

    Bug fixing

--- src/validators/getLoginValidationSchema.ts
+++ src/validators/getLoginValidationSchema.ts
@@ -1,28 +1,16 @@
 import { LoginViewModel } from '@/view/models';
-import { UseValidationTranslationReturnValue } from '@atom/common';
+import { regexLibrary, UseValidationTranslationReturnValue } from '@atom/common';
 import { object, SchemaOf, string } from 'yup';
 
 export const getLoginValidationSchema = async (
   t: UseValidationTranslationReturnValue
 ): Promise<SchemaOf<Omit<LoginViewModel, 'returnUrl'>>> => {
   return object({
-    email: string()
-      .required(t.required())
-      .max(40, t.max(40))
-      .min(6, t.min(6))
-      .matches(
-        // eslint-disable-next-line no-useless-escape
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Invalid Email'
-      ),
+    email: string().email(t.email()).min(3, t.max(6)).max(30, t.max(30)).required(t.required()),
     password: string()
       .min(8, t.min(8))
       .max(55, t.max(55))
       .required(t.required())
-      // eslint-disable-next-line no-useless-escape
-      .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-        'Password must include both lower and upper case letters , numbers and symbols'
-      )
+      .matches(regexLibrary.PASSWORD_INPUT, t.password())
   });
 };
